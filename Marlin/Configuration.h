fc7d568ed67072e0c688b8861a9a13fad29ef4c1
Domingos Rodrigues <Drodrigues@beeverycreative.com>
2017-07-24 16:54:50 +0100

    Refined Acceleration and Jerk setting
    Resolves ringing problems and further enhances layer consistency and printing quality.
    Also reduced extruder velocity to avoid too fast retractions that cause jams

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -491,7 +491,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 300, 300, 2, 50 }
+#define DEFAULT_MAX_FEEDRATE          { 200, 200, 2, 20 }
 
 /**
  * Default Max Acceleration (change/s) change = mm/s
@@ -499,7 +499,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 1000, 1000, 100, 10000 }
+#define DEFAULT_MAX_ACCELERATION      { 1000, 1000, 10, 10000 }
 
 /**
  * Default Acceleration (change/s) change = mm/s
@@ -510,8 +510,8 @@
  *   M204 T    Travel Acceleration
  */
 #define DEFAULT_ACCELERATION          1000    // X, Y, Z and E acceleration for printing moves
-#define DEFAULT_RETRACT_ACCELERATION  3000    // E acceleration for retracts
-#define DEFAULT_TRAVEL_ACCELERATION   3000    // X, Y, Z acceleration for travel (non printing) moves
+#define DEFAULT_RETRACT_ACCELERATION  2000    // E acceleration for retracts
+#define DEFAULT_TRAVEL_ACCELERATION   2000    // X, Y, Z acceleration for travel (non printing) moves
 
 /**
  * Default Jerk (mm/s)
@@ -521,9 +521,9 @@
  * When changing speed and direction, if the difference is less than the
  * value set here, it may happen instantaneously.
  */
-#define DEFAULT_XJERK                 20.0
-#define DEFAULT_YJERK                 15.0
-#define DEFAULT_ZJERK                  0.4
+#define DEFAULT_XJERK                  5.0
+#define DEFAULT_YJERK                  5.0
+#define DEFAULT_ZJERK                  0.0
 #define DEFAULT_EJERK                  5.0
 
 
