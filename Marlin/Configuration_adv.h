3d3a744c14fc502a998440fa4d9076da51b1c1e9
Domingos Rodrigues <Drodrigues@beeverycreative.com>
2018-06-22 13:39:29 +0100

    Increased homing sensitivity to avoid unnecessary forcing

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1179,8 +1179,8 @@
    */
   //#define SENSORLESS_HOMING // TMC2130 only
 
-    #define X_HOMING_SENSITIVITY  10
-    #define Y_HOMING_SENSITIVITY  10
+    #define X_HOMING_SENSITIVITY  9
+    #define Y_HOMING_SENSITIVITY  9
 
   /**
    * Enable M122 debugging command for TMC stepper drivers.
