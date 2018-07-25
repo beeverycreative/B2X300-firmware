3431db9a504ed29130ce40a0e1456897e897a209
Domingos Rodrigues <Drodrigues@beeverycreative.com>
2018-07-25 11:03:20 +0100

    Changed default stepper driver current

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1084,16 +1084,16 @@
    */
 
   #define R_SENSE           0.11  // R_sense resistor for SilentStepStick2130
-  #define HOLD_MULTIPLIER    0.5  // Scales down the holding current from run current
+  #define HOLD_MULTIPLIER    0.7  // Scales down the holding current from run current
   #define INTERPOLATE       true  // Interpolate X/Y/Z_MICROSTEPS to 256
 
-  #define X_CURRENT          800  // rms current in mA. Multiply by 1.41 for peak current.
+  #define X_CURRENT          900  // rms current in mA. Multiply by 1.41 for peak current.
   #define X_MICROSTEPS        16  // 0..256
 
-  #define Y_CURRENT          800
+  #define Y_CURRENT          900
   #define Y_MICROSTEPS        16
 
-  #define Z_CURRENT          800
+  #define Z_CURRENT          925
   #define Z_MICROSTEPS        16
 
   #define X2_CURRENT         800
@@ -1105,10 +1105,10 @@
   #define Z2_CURRENT         800
   #define Z2_MICROSTEPS       16
 
-  #define E0_CURRENT         800
+  #define E0_CURRENT         925
   #define E0_MICROSTEPS       16
 
-  #define E1_CURRENT         800
+  #define E1_CURRENT         925
   #define E1_MICROSTEPS       16
 
   #define E2_CURRENT         800
