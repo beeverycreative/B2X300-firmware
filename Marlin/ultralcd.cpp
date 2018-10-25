189dc95e851f38644ef0c2039762b97975b26361
Domingos Rodrigues <Drodrigues@beeverycreative.com>
2018-10-25 09:58:28 +0100

    Fixed Nozzle Z-offset text

--- Marlin/ultralcd.cpp
+++ Marlin/ultralcd.cpp
@@ -782,45 +782,37 @@ uint16_t max_display_update_time = 0;
 
   void lcd_screen_dual_z_offset_start() {
     START_SCREEN();
-    STATIC_ITEM(_UxGT("Dual Z offset test"), true, true);
-    STATIC_ITEM("This assistant will",false,false);
-    STATIC_ITEM("help you to adjust",false,false);
-    STATIC_ITEM("the height of both",false,false);
+    STATIC_ITEM(_UxGT("Nozzle Z-offset test"), true, true);
+    STATIC_ITEM("Test the height",false,false);
+    STATIC_ITEM("offset between both",false,false);
+    STATIC_ITEM("nozzles.",false,false);
     STATIC_ITEM("(scroll to read more)",false,false);
     STATIC_ITEM("---------------------",false,false);
-    STATIC_ITEM("nozzles to allow",false,false);
-    STATIC_ITEM("dual extrusion to",false,false);
-    STATIC_ITEM("work correctly.",false,false);
-    STATIC_ITEM("Ensure both nozzles",false,false);
-    STATIC_ITEM("are using the same",false,false);
-    STATIC_ITEM("material from the",false,false);
-    STATIC_ITEM("same brand, otherwise",false,false);
-    STATIC_ITEM("the test will show",false,false);
-    STATIC_ITEM("incorrect results.",false,false);
-    STATIC_ITEM(" ");
+    STATIC_ITEM("This procedure will",false,false);
+    STATIC_ITEM("help you assert if",false,false);
+    STATIC_ITEM("the 2 nozzles are at",false,false);
+    STATIC_ITEM("the same height",false,false);
+    STATIC_ITEM("relative to the bed.",false,false);
+    STATIC_ITEM(" ",false,false);
     STATIC_ITEM("Please check the",false,false);
-    STATIC_ITEM("'Calibration' chapter",false,false);
+    STATIC_ITEM("'Other Codes' chapter",false,false);
     STATIC_ITEM("on the User Manual for",false,false);
     STATIC_ITEM("more information.",false,false);
+    STATIC_ITEM(" ");
+    STATIC_ITEM("Proc. code: ST81 ", false,false);
     STATIC_ITEM(" ");
     STATIC_ITEM("Click to continue.",false,false);
     END_SCREEN();
   }
 
-  void lcd_dual_z_offset_hotend_status() {
+  void lcd_nozzle_z_offset_hotend_status() {
 		START_SCREEN();
-      STATIC_ITEM(_UxGT("Dual Z offset test"), true, true);
-      STATIC_ITEM(MSG_FILAMENT_CHANGE_HEATING_1 "  ", true, false);
+      STATIC_ITEM(_UxGT("Nozzle Z-offset test"), true, true);
       STATIC_ITEM("  ", false, false);
       STATIC_ITEM("  ", false, false);
+      STATIC_ITEM("Status: heating E1+E2 ", false, false);
       #ifdef MSG_FILAMENT_CHANGE_HEATING_2
-        STATIC_ITEM(MSG_FILAMENT_CHANGE_HEATING_2 "  ", true, false);
-        #define _FC_LINES_C 3
-      #else
-        #define _FC_LINES_C 2
-      #endif
-      #if LCD_HEIGHT > _FC_LINES_C + 1
-        STATIC_ITEM(" ");
+        STATIC_ITEM("Please wait. ", false, false);
       #endif
 
 
@@ -849,7 +841,7 @@ uint16_t max_display_update_time = 0;
       lcd_printPGM(PSTR(LCD_STR_DEGREE));
       u8g.print("C");
 	  #else
-		  u8g.setPrintPos(0, 36);
+		  u8g.setPrintPos(0, 24);
 			u8g.print("Nozzle E1: ");
 
       if(round(thermalManager.degHotend(0)) <100)
@@ -861,7 +853,7 @@ uint16_t max_display_update_time = 0;
       lcd_printPGM(PSTR(LCD_STR_DEGREE));
       u8g.print("C");
 
-      u8g.setPrintPos(0, 48);
+      u8g.setPrintPos(0, 36);
 			u8g.print("Nozzle E2: ");
 
       if(round(thermalManager.degHotend(1)) <100)
@@ -877,20 +869,14 @@ uint16_t max_display_update_time = 0;
       END_SCREEN();
 	}
 
-  void lcd_dual_z_offset_bed_status() {
+  void lcd_nozzle_z_offset_bed_status() {
 		START_SCREEN();
-      STATIC_ITEM(_UxGT("Dual Z offset test"), true, true);
-      STATIC_ITEM(_UxGT("Heating bed"), true, false);
+      STATIC_ITEM(_UxGT("Nozzle Z-offset test"), true, true);
       STATIC_ITEM("  ", true, false);
+      STATIC_ITEM(_UxGT("Status: heating bed"), false, false);
       STATIC_ITEM("  ", true, false);
       #ifdef MSG_FILAMENT_CHANGE_HEATING_2
-        STATIC_ITEM(MSG_FILAMENT_CHANGE_HEATING_2 "  ", true, false);
-        #define _FC_LINES_C 3
-      #else
-        #define _FC_LINES_C 2
-      #endif
-      #if LCD_HEIGHT > _FC_LINES_C + 1
-        STATIC_ITEM(" ");
+        STATIC_ITEM("Please wait. ", false, false);
       #endif
 
 
@@ -908,7 +894,7 @@ uint16_t max_display_update_time = 0;
       u8g.print("C");
 
 	  #else
-		  u8g.setPrintPos(16, 36);
+		  u8g.setPrintPos(16, 24);
 			u8g.print("Bed: ");
 
 			if(round(thermalManager.degBed()) <100)
@@ -924,9 +910,9 @@ uint16_t max_display_update_time = 0;
       END_SCREEN();
 	}
 
-  void lcd_dual_z_offset_homing() {
+  void lcd_nozzle_z_offset_homing() {
     START_SCREEN();
-    STATIC_ITEM(_UxGT("Dual Z offset test"), true, true);
+    STATIC_ITEM(_UxGT("Nozzle Z-offset test"), true, true);
     STATIC_ITEM(_UxGT("Status: finding mesh"), false, false);
     STATIC_ITEM(_UxGT(" "));
     STATIC_ITEM(_UxGT(" "));
@@ -935,9 +921,9 @@ uint16_t max_display_update_time = 0;
     END_SCREEN();
   }
 
-  void lcd_dual_z_offset_printing() {
+  void lcd_nozzle_z_offset_printing() {
     START_SCREEN();
-    STATIC_ITEM(_UxGT("Dual Z offset test"), true, true);
+    STATIC_ITEM(_UxGT("Nozzle Z-offset test"), true, true);
     STATIC_ITEM(_UxGT("Status: printing"), false, false);
     STATIC_ITEM(_UxGT(" "));
     STATIC_ITEM(_UxGT(" "));
@@ -946,13 +932,13 @@ uint16_t max_display_update_time = 0;
     END_SCREEN();
   }
 
-  void lcd_dual_z_offset_choose_result_ok() {
+  void lcd_nozzle_z_offset_choose_result_ok() {
     START_SCREEN();
-    STATIC_ITEM(_UxGT("Dual Z offset test"), true, true);
-    STATIC_ITEM(_UxGT("Status: Successful"), false, false);
+    STATIC_ITEM(_UxGT("Nozzle Z-offset test"), true, true);
+    STATIC_ITEM(_UxGT("Status: successful"), false, false);
     STATIC_ITEM(_UxGT("You can now calibrate"), false, false);
     STATIC_ITEM(_UxGT("the XY offset. "), false, false);
-    STATIC_ITEM(_UxGT("Click to continue."), false, false);
+    STATIC_ITEM(_UxGT("Click to exit."), false, false);
 
     END_SCREEN();
 
@@ -960,20 +946,21 @@ uint16_t max_display_update_time = 0;
     beevc_continue = true;
   }
 
-  void lcd_dual_z_offset_choose_result_nok() {
+  void lcd_nozzle_z_offset_choose_result_nok() {
     START_SCREEN();
-    STATIC_ITEM(_UxGT("Dual Z offset test"), true, true);
-    STATIC_ITEM(_UxGT("Status: NOK"), false, false);
-    STATIC_ITEM(_UxGT("Please, check the"), false, false);
-    STATIC_ITEM(_UxGT("'Other Codes' chapter"), false, false);
+    STATIC_ITEM(_UxGT("Nozzle Z-offset test"), true, true);
+    STATIC_ITEM(_UxGT("Status: bad offset"), false, false);
+    STATIC_ITEM(_UxGT("Nozzles are not "), false, false);
+    STATIC_ITEM(_UxGT("parallel to the bed!"), false, false);
     STATIC_ITEM(_UxGT("(scroll to read more)"), false, false);
     STATIC_ITEM(_UxGT("---------------------"), false, false);
-    STATIC_ITEM(_UxGT("on the User Manual for"), false, false);
-    STATIC_ITEM(_UxGT("more information."), false, false);
+    STATIC_ITEM(_UxGT("Please recalibrate "), false, false);
+    STATIC_ITEM(_UxGT("according to the"), false, false);
+    STATIC_ITEM(_UxGT("following procedure!"), false, false);
     STATIC_ITEM(_UxGT(" "), false, false);
     STATIC_ITEM(_UxGT("Proc. code: ST81"), false, false);
     STATIC_ITEM(_UxGT(" "), false, false);
-    STATIC_ITEM(_UxGT("Click to continue."), false, false);
+    STATIC_ITEM(_UxGT("Click to exit."), false, false);
 
     END_SCREEN();
 
@@ -981,22 +968,22 @@ uint16_t max_display_update_time = 0;
     beevc_continue = true;
   }
 
-  void lcd_dual_z_offset_choose_result(){
+  void lcd_nozzle_z_offset_choose_result(){
     START_MENU();
 
-    STATIC_ITEM(_UxGT("Dual Z offset test"), true, true);
+    STATIC_ITEM(_UxGT("Nozzle Z-offset test"), true, true);
 
-    STATIC_ITEM(_UxGT("Is the result ok?"), true, false);
+    STATIC_ITEM(_UxGT("Lines are similar?"), true, false);
 
-    MENU_ITEM(submenu, _UxGT(" - Yes"), lcd_dual_z_offset_choose_result_ok);
-    MENU_ITEM(submenu, _UxGT(" - No"), lcd_dual_z_offset_choose_result_nok);
+    MENU_ITEM(submenu, _UxGT(" - Yes"), lcd_nozzle_z_offset_choose_result_ok);
+    MENU_ITEM(submenu, _UxGT(" - No"), lcd_nozzle_z_offset_choose_result_nok);
 
     END_MENU();
   }
 
-  void lcd_dual_z_offset_run () {
+  void lcd_nozzle_z_offset_run () {
     // Shows moving screen
-    lcd_goto_screen(lcd_dual_z_offset_printing);
+    lcd_goto_screen(lcd_nozzle_z_offset_printing);
 
     // Force screen update
     beevc_force_screen_update();
@@ -1008,7 +995,7 @@ uint16_t max_display_update_time = 0;
     beevc_continue = false;
 
     // Go to question screen
-    lcd_goto_screen(lcd_dual_z_offset_choose_result);
+    lcd_goto_screen(lcd_nozzle_z_offset_choose_result);
 
     // Waits for continue
     while (!beevc_continue){
@@ -1026,9 +1013,9 @@ uint16_t max_display_update_time = 0;
 
   }
 
-  void lcd_dual_z_offset_home_level() {
+  void lcd_nozzle_z_offset_home_level() {
     // Show finding mesh screen
-    lcd_goto_screen(lcd_dual_z_offset_homing);
+    lcd_goto_screen(lcd_nozzle_z_offset_homing);
 
     // Force screen update
     beevc_force_screen_update();
@@ -1038,12 +1025,12 @@ uint16_t max_display_update_time = 0;
     gcode_G29();
 
     // Next screen
-    lcd_dual_z_offset_run();
+    lcd_nozzle_z_offset_run();
   }
 
-  void lcd_dual_z_offset_wait_for_temp_hotend() {
+  void lcd_nozzle_z_offset_wait_for_temp_hotend() {
     // Show "wait for heating" Hotends
-    lcd_goto_screen(lcd_dual_z_offset_hotend_status);
+    lcd_goto_screen(lcd_nozzle_z_offset_hotend_status);
 
     // Prepares the temporary variables
     unsigned long next_update = millis() + 200;
@@ -1067,7 +1054,7 @@ uint16_t max_display_update_time = 0;
       }
 
       // Show "wait for heating" bed
-      lcd_goto_screen(lcd_dual_z_offset_bed_status);
+      lcd_goto_screen(lcd_nozzle_z_offset_bed_status);
 
       // Force screen update
       beevc_force_screen_update();
@@ -1093,49 +1080,49 @@ uint16_t max_display_update_time = 0;
             }
         }
 
-    lcd_dual_z_offset_home_level();
+    lcd_nozzle_z_offset_home_level();
   }
 
-  void lcd_dual_z_offset_pla(){
+  void lcd_nozzle_z_offset_pla(){
     filament_change_temp = 210;
     thermalManager.setTargetHotend(filament_change_temp, 0);
     thermalManager.setTargetHotend(filament_change_temp, 1);
     thermalManager.setTargetBed(50);
-    lcd_goto_screen(lcd_dual_z_offset_wait_for_temp_hotend);
+    lcd_goto_screen(lcd_nozzle_z_offset_wait_for_temp_hotend);
   }
 
-  void lcd_dual_z_offset_petg(){
+  void lcd_nozzle_z_offset_petg(){
     filament_change_temp = 230;
     thermalManager.setTargetHotend(filament_change_temp, 0);
     thermalManager.setTargetHotend(filament_change_temp, 1);
     thermalManager.setTargetBed(70);
-    lcd_goto_screen(lcd_dual_z_offset_wait_for_temp_hotend);
+    lcd_goto_screen(lcd_nozzle_z_offset_wait_for_temp_hotend);
   }
 
-  void lcd_dual_z_offset_abs(){
+  void lcd_nozzle_z_offset_abs(){
     filament_change_temp = 240;
     thermalManager.setTargetHotend(filament_change_temp, 0);
     thermalManager.setTargetHotend(filament_change_temp, 1);
     thermalManager.setTargetBed(100);
-    lcd_goto_screen(lcd_dual_z_offset_wait_for_temp_hotend);
+    lcd_goto_screen(lcd_nozzle_z_offset_wait_for_temp_hotend);
   }
 
-  void lcd_dual_z_offset_choose_material(){
+  void lcd_nozzle_z_offset_choose_material(){
     START_MENU();
 
-    STATIC_ITEM(_UxGT("Dual Z offset test"), true, true);
+    STATIC_ITEM(_UxGT("Nozzle Z-offset test"), true, true);
 
     // Go back to previous menu
     MENU_BACK(MSG_BACK);
 
-    MENU_ITEM(submenu, _UxGT("PLA   210ºC"), lcd_dual_z_offset_pla);
-    MENU_ITEM(submenu, _UxGT("PETG  230ºC"), lcd_dual_z_offset_petg);
-    MENU_ITEM(submenu, _UxGT("ABS   240ºC"), lcd_dual_z_offset_abs);
+    MENU_ITEM(submenu, _UxGT("PLA   210ºC"), lcd_nozzle_z_offset_pla);
+    MENU_ITEM(submenu, _UxGT("PETG  230ºC"), lcd_nozzle_z_offset_petg);
+    MENU_ITEM(submenu, _UxGT("ABS   240ºC"), lcd_nozzle_z_offset_abs);
 
     END_MENU();
   }
 
-  void lcd_dual_z_offset_start(){
+  void lcd_nozzle_z_offset_start(){
     // Shows start screen
     lcd_goto_screen(lcd_screen_dual_z_offset_start);
 
@@ -1143,7 +1130,7 @@ uint16_t max_display_update_time = 0;
     beevc_wait_click();
 
     // Shows start screen
-    lcd_goto_screen(lcd_dual_z_offset_choose_material);
+    lcd_goto_screen(lcd_nozzle_z_offset_choose_material);
   }
 
 
@@ -1659,7 +1646,7 @@ void kill_screen(const char* lcd_msg) {
         //MENU_ITEM(submenu, _UxGT("Cold pull"), _lcd_menu_z_offset);
 
       // Calibrate Dual nozzle Z offset
-        MENU_ITEM(submenu, _UxGT("Dual Z offset test"), lcd_dual_z_offset_start);
+        MENU_ITEM(submenu, _UxGT("Nozzle Z-offset test"), lcd_nozzle_z_offset_start);
 
       // Auto Home/ Level Bed
         // Leveling only appears when automatic bed leveling method exists
